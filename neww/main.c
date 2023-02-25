251d138381507df9431d4bc91ad24abae825961b
Aditya Kashyap <aditya.kashyap0215@gmail.com>
2023-02-25 14:54:56 +0530

    seesaw and slide

--- neww/main.c
+++ neww/main.c
@@ -8,21 +8,151 @@ void init(){
     glMatrixMode(GL_PROJECTION);
     glOrtho(0,50,0,50,0,10);
 }
-char str[16]="CHILDREN'S PARK";
+char str[18]="CHILDREN'S  PARK";
 char name[5]="NAME";
 char usn[4]="USN";
-char adi[15]="ADITYA KASHYAP";
-char adv[12]="ADVIKA BHAT";
+char adi[16]="ADITYA  KASHYAP";
+char adv[13]="ADVIKA  BHAT";
 char adiusn[11]="4NM20CS014";
 char advusn[11]="4NM20CS018";
 char next[5]="NEXT";
 char prev[5]="PREV";
-char ugf[24]="UNDER THE GUIDANCE OF:-";
-char sir[18]="DR.PRADEEP KANCHAN";
-char sir2[15]="MR.PUNEETH R P";
+char ugf[28]="UNDER  THE  GUIDANCE  OF:-";
+char sir[20]="DR. PRADEEP  KANCHAN";
+char sir2[18]="MR. PUNEETH  R  P";
 
+
+
+/*void cube(){
+    glBegin(GL_QUADS);
+     glColor3f(1.0,1.0,1.0);
+    glVertex3i(20,20,20);
+    glVertex3i(20,-20,20);
+    glVertex3i(-20,-20,20);
+    glVertex3i(-20,20,20);
+
+
+      glColor3f(0.0,1.0,0.0);
+    glVertex3i(20,20,-20);
+    glVertex3i(20,-20,-20);
+    glVertex3i(-20,-20,-20);
+    glVertex3i(-20,20,-20);
+
+
+      glColor3f(1.0,0.0,0.0);
+    glVertex3i(20,20,20);
+    glVertex3i(20,20,-20);
+    glVertex3i(-20,20,-20);
+    glVertex3i(-20,20,20);
+
+
+      glColor3f(0.0,1.0,1.0);
+    glVertex3i(20,20,20);
+    glVertex3i(20,20,-20);
+    glVertex3i(-20,20,-20);
+    glVertex3i(-20,20,20);
+
+
+      glColor3f(1.0,1.0,0.0);
+    glVertex3i(20,20,20);
+    glVertex3i(20,-20,20);
+    glVertex3i(20,-20,-20);
+    glVertex3i(20,20,-20);
+
+
+      glColor3f(1.0,0.0,1.0);
+    glVertex3i(-20,20,20);
+    glVertex3i(-20,-20,20);
+    glVertex3i(-20,-20,-20);
+    glVertex3i(-20,20,-20);
+
+
+    glEnd();
+    glFlush();
+
+    }
+*/
+
+
+void slide(){
+    glColor3f(0.7,0.6,0.8);
+        glLineWidth(10);
+
+    glBegin(GL_LINES);
+    glVertex2i(32,25);
+    glVertex2i(32,10);
+    glVertex2i(35,21);
+    glVertex2i(35,10);
+
+
+    glVertex2i(32,20);
+    glVertex2i(35,20);
+
+    glVertex2i(32,17);
+    glVertex2i(35,17);
+
+    glVertex2i(32,14);
+    glVertex2i(35,14);
+
+    glEnd();
+
+
+
+
+    glBegin(GL_POLYGON);
+    glVertex2i(32,25);
+    glVertex2i(37,25);
+    glVertex2i(42,10);
+    glVertex2i(47,10);
+    glVertex2i(32,25);
+    glVertex2i(42,10);
+    glVertex2i(47,10);
+    glVertex2i(37,25);
+
+    glEnd();
+    glFlush();
+
+
+}
+
+void seesaw(){
+    glColor3f(1.0,0.8,0.4);
+    glLineWidth(20);
+    glBegin(GL_LINES);
+    glVertex2i(31,43);
+    glVertex2i(47,39);
+    glVertex2i(39,41);
+    glVertex2i(39,38);
+    glEnd();
+    glBegin(GL_LINES);
+    glLineWidth(10);
+    glVertex2i(33,43);
+    glVertex2i(33,44);
+    glVertex2i(45,41);
+    glVertex2i(45,40);
+
+
+    glFlush();
+
+
+
+
+}
 void displayNewWin(){
     glClear(GL_COLOR_BUFFER_BIT);
+
+
+
+      seesaw();
+    glEnd();
+    glFlush();
+
+
+    slide();
+    glEnd();
+    glFlush();
+
+
     glColor3f(1,0,0);
     glBegin(GL_POLYGON);
         glVertex2i(20,5);
@@ -32,13 +162,16 @@ void displayNewWin(){
     glEnd();
     glFlush();
 
-    for(int i=0;i<strlen(next);i++){
+
     glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
-    glRasterPos2i(21+i,6);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,prev[i]);
-    }
+    glRasterPos2i(22,6);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,prev);
      glEnd();
     glFlush();
+
+
+
+
 }
 
 void OnMouseClickNew    (int button, int state, int x, int y)
@@ -75,7 +208,7 @@ void OnMouseClick(int button, int state, int x, int y)
 {
   if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN)
   {
-     printf("%d %d\n",x,y);
+    // printf("%d %d\n",x,y);
      if(x>=615 && x<=800 && y>=710 && y<=760){
         playground();
      }
@@ -87,62 +220,56 @@ void display(){
     glClear(GL_COLOR_BUFFER_BIT);
 
     //making button
-
-    for(int i=0;i<15;i++){
     glColor3f(1.0,1.0,1.0);
-    glRasterPos2i(15+i,45);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,str[i]);
-    }
+    glRasterPos2f(20,45);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,str);
 
 
-     for(int i=0;i<4;i++){
+
     glColor3f(1.0,0.0,0.0);
-    glRasterPos2i(6+i,35);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,name[i]);
-    }
+    glRasterPos2i(7,35);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,name);
 
-    for(int i=0;i<3;i++){
+
     glColor3f(1.0,0.0,0.0);
-    glRasterPos2i(36+i,35);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,usn[i]);
-    }
-    for(int i=0;i<15;i++){
-    glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
-    glRasterPos2i(5+i,30);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,adi[i]);
-    }
-    for(int i=0;i<10;i++){
-    glColor4f(0.0f, 1.0f, 1.0f, 1.0f);
-    glRasterPos2i(35+i,30);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,adiusn[i]);
-    }
-       for(int i=0;i<11;i++){
-    glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
-    glRasterPos2i(5+i,25);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,adv[i]);
-    }
-    for(int i=0;i<10;i++){
-    glColor4f(1.0f, 1.0f, 0.0f, 0.0f);
-    glRasterPos2i(35+i,25);
-    glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,advusn[i]);
-    }
-     for(int i=0;i<24;i++){
-    glColor3f(0.5,0.8,0.4);
-    glRasterPos2i(5+i,20);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,ugf[i]);
-    }
-      for(int i=0;i<18;i++){
-    glColor3f(1.0,1.0,1.0);
-    glRasterPos2i(29+i,20);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,sir[i]);
-    }
+    glRasterPos2i(36,35);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,usn);
+
+     glColor3f(0.5,0.8,0.3);
+    glRasterPos2i(5,30);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,adi);
 
 
-      for(int i=0;i<15;i++){
+     glColor3f(0.5,0.8,0.3);
+    glRasterPos2i(35,30);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,adiusn);
+
+     glColor3f(0.0,1.0,1.0);
+    glRasterPos2i(5,25);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,adv);
+
+    glColor3f(0.0,1.0,1.0);
+    glRasterPos2i(35,25);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,advusn);
+
+
+
+
+     glColor3f(0.4,0.5,0.8);
+    glRasterPos2i(18,20);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,ugf);
+
+
     glColor3f(1.0,1.0,1.0);
-    glRasterPos2i(29+i,15);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,sir2[i]);
-    }
+    glRasterPos2i(5,15);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,sir);
+
+
+
+    glColor3f(1.0,1.0,1.0);
+    glRasterPos2i(35,15);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,sir2);
+
 
 
 
@@ -155,18 +282,18 @@ void display(){
     glEnd();
     glFlush();
 
-    for(int i=0;i<strlen(next);i++){
+
+
     glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
-    glRasterPos2i(21+i,6);
-    glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,next[i]);
-    }
+    glRasterPos2i(22,6);
+    glutBitmapString(GLUT_BITMAP_TIMES_ROMAN_24,next);
 
     glLineWidth(10);
     glColor3f(0.2,0.5,0.8);
 
     glBegin(GL_LINES);
-    glVertex2i(13,43);
-    glVertex2i(31,43);
+    glVertex2i(17,43);
+    glVertex2i(30,43);
 
     glEnd();
     glFlush();
